7d5969e2358d9725e218dc0ee65108f350b273ab
shreyakshirsagar097-crypto <shreyakshirsagar097@gmail.com>
2026-09-07 10:41:21 -0700

    Create 04_Data Types_output.cpp

---
+++ 04_Data Types_output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int age = 20;
+  float marks = 95.5;
+  double salary = 75000.00;
+  char grade  = 'A';
+  bool passed = true;
+
+cout<<"Age:"<<age<<endl;
+cout<<"Marks:"<<marks<<endl;
+cout<<"Salary:"<<salary<<endl;
+cout<<"grade:"<<grade<<endl;
+cout<<"passed"<<passed<<endl;
+
+return 0;
+  
+}
