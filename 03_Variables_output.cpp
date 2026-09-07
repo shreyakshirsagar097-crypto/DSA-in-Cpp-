a3774f0558e032713d2dbe2c11e773c72242df4e
shreyakshirsagar097-crypto <shreyakshirsagar097@gmail.com>
2026-09-07 10:03:04 -0700

    Create 03_Variables_output.cpp

---
+++ 03_Variables_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+  int a = 10;
+  int b = 20;
+  int sum = a + b;
+
+cout<< "Enter the value of a:"<< a <<endl;
+cout<< " Enter the value of b:" << b << endl;
+cout<< " sum :" << endl;
+
+
+return 0;
+}
